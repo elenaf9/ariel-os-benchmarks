86877b6b1a84fb0622f4a2a42a257a9fe58da869
Elena Frank <elena.frank@protonmail.com>
2025-04-07 22:50:20 +1200

    benchmarks: readd bench_thread_flags_preempt

---
+++ benchmarks/bench_thread_flags_preempt/README.md
@@ -0,0 +1,12 @@
+# bench_thread_flags_preempt
+
+## About
+
+This benchmark tests the context switching performance in case of a preemption.
+Two threads are running: one higher priority one that is just waiting for it's flag to be set, and a lower priority one that sets the flag and is then being preempted
+
+## How to run
+
+In this folder, run
+
+    laze build -b rpi-pico  -s <FEAT> -s <REV> run
---
+++ benchmarks/bench_thread_flags_preempt/src/main.rs
@@ -0,0 +1,32 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+
+use ariel_os::{
+    debug::log::*,
+    thread::{current_tid, sync::Channel, thread_flags, ThreadId},
+};
+
+static ID_EXCHANGE: Channel<ThreadId> = Channel::new();
+
+#[ariel_os::thread(autostart, priority = 1)]
+fn thread0() {
+    let target_pid = ID_EXCHANGE.recv();
+
+    match bench_multicore::benchmark(10000, || {
+        thread_flags::set(target_pid, 1);
+    }) {
+        Ok(ticks) => info!("took {} ticks per iteration", ticks),
+        Err(err) => error!("benchmark error: {}", err),
+    }
+    loop {}
+}
+
+#[ariel_os::thread(autostart, priority = 2)]
+fn thread1() {
+    ID_EXCHANGE.send(&current_tid().unwrap());
+    loop {
+        thread_flags::wait_all(1);
+    }
+}
